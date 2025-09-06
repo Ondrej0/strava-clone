1cc811f946cd75bf6c83f591f0e1cc3a71fd2b81
Ondrej0 <ondrejmuzik27@gmail.com>
2025-09-06 13:10:26 +0100

    jj

--- src/AddRun.tsx
+++ src/AddRun.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
